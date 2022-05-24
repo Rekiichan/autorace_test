4b9e292bddda13d9af8388a48c955150d4d30972
rekiichan <dtnhan.ete.dut@gmail.com>
2022-05-24 08:16:59 +0700

    fourth

--- src/Line_Process.cpp
+++ src/Line_Process.cpp
@@ -2,43 +2,320 @@
 #include "Line_Process.h"
 #include "main.h"
 
-void LinePIDFilter()
-{ // PID control in line tracking mode
-    static double kP = 39, kI = 0.005, kD = 30;
-    static double gain = 100;
-    static double prev_error = 0, prev_I = 0;
-    double p = 0, i = 0, d = 0, pid_value;
-
-    p = GetError();
-    i = p + prev_I;
-    d = p - prev_error;
-
-    pid_value = kP * p + kI * i + kD * d;
-    prev_I = i;
-    prev_error = p;
-    pid_value *= gain / 100;
-    Motor[0] = INITIAL_SPEED + pid_value;
-    Motor[1] = INITIAL_SPEED - pid_value;
+int GetError()
+{ // Get the "error" in the moving direction
+  static int prev_error = 0;
+  switch (HighSignalCount())
+  {
+  case 0:
+  {
+    // CheckModeSwitch();
+    if (prev_error == 0)
+    {
+      return 0;
+    }
+    else if (prev_error == 6 || prev_error == 7)
+    {
+      prev_error = 7;
+      return 7;
+    }
+    else if (prev_error == -6 || prev_error == -7)
+    {
+      prev_error = -7;
+      return -7;
+    }
+    else
+      return prev_error;
+    break;
+  }
+  case 1:
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      if (Line[i])
+      {
+        switch (i)
+        {
+        case 0:
+        {
+          prev_error = -6;
+          return -6;
+          break;
+        }
+        case 1:
+        {
+          prev_error = -4;
+          return -4;
+          break;
+        }
+        case 2:
+        {
+          prev_error = -2;
+          return -2;
+          break;
+        }
+        case 3:
+        {
+          prev_error = 0;
+          return 0;
+          break;
+        }
+        case 4:
+        {
+          prev_error = 2;
+          return 2;
+          break;
+        }
+        case 5:
+        {
+          prev_error = 4;
+          return 4;
+          break;
+        }
+        case 6:
+        {
+          prev_error = 6;
+          return 6;
+          break;
+        }
+        default:
+        {
+          return prev_error;
+          break;
+        }
+        }
+        break;
+      }
+    }
+    break;
+  }
+  case 2:
+  {
+    if (Line[0] && Line[1])
+    {
+      prev_error = -5;
+      return -5;
+    }
+    else if (Line[1] && Line[3])
+    {
+      prev_error = -7;
+      return -7;
+    }
+    else if (Line[1] && Line[2])
+    {
+      prev_error = -3;
+      return -3;
+    }
+    else if (Line[2] && Line[3])
+    {
+      prev_error = -1;
+      return -1;
+    }
+    else if (Line[3] && Line[4])
+    {
+      prev_error = 1;
+      return 1;
+    }
+    else if (Line[4] && Line[5])
+    {
+      prev_error = 3;
+      return 3;
+    }
+    else if (Line[5] && Line[3])
+    {
+      prev_error = 7;
+      return 7;
+    }
+    else if (Line[5] && Line[6])
+    {
+      prev_error = 5;
+      return 5;
+    }
+    else
+      return prev_error;
+    break;
+  }
+  case 3:
+  {
+    if (Line[1] && Line[2] && Line[3])
+    {
+      switch (-1)
+      {
+      case 0:
+      {
+        prev_error = 0;
+        return 0;
+      }
+      case -1:
+      case 1:
+      {
+        prev_error = -4;
+        return -4;
+      }
+      }
+    }
+    else if (Line[0] && Line[1] && Line[2])
+    {
+      prev_error = -6;
+      return -6;
+    }
+    else if (Line[3] && Line[4] && Line[5])
+    {
+      switch (1)
+      {
+      case 0:
+      {
+        prev_error = 0;
+        return 0;
+      }
+      case 1:
+      case -1:
+      {
+        prev_error = 4;
+        return 4;
+      }
+      }
+    }
+    else if (Line[4] && Line[5] && Line[6])
+    {
+      prev_error = 6;
+      return 6;
+    }
+    else if (Line[2] && Line[3] && Line[4])
+    {
+      prev_error = 0;
+      return 0;
+    }
+    else
+      return prev_error;
+    break;
+  }
+  case 4:
+  {
+    if (Line[0] && Line[1] && Line[2] && Line[3])
+    {
+      switch (CheckBarrie(-1))
+      {
+      case 0:
+      {
+        prev_error = 0;
+        return 0;
+      }
+      case -1:
+      case 1:
+      {
+        prev_error = -8;
+        return -8;
+      }
+      }
+    }
+    else if (Line[6] && Line[5] && Line[4] && Line[3])
+    {
+      switch (CheckBarrie(1))
+      {
+      case 0:
+      {
+        prev_error = 0;
+        return 0;
+      }
+      case 1:
+      case -1:
+      {
+        prev_error = 8;
+        return 8;
+      }
+      }
+    }
+    else
+    {
+      return prev_error;
+    }
+    break;
+  }
+  case 5:
+  {
+    if (Line[0] && Line[1] && Line[2] && Line[3] && Line[4])
+    {
+      prev_error = -2;
+      return -2;
+    }
+    else if (Line[1] && Line[2] && Line[3] && Line[4] && Line[5])
+    {
+      prev_error = 0;
+      return 0;
+    }
+    else if (Line[2] && Line[3] && Line[4] && Line[5] && Line[6])
+    {
+      prev_error = 2;
+      return 2;
+    }
+  }
+  case 6:
+  {
+    if (Line[0] && Line[1] && Line[2] && Line[3] && Line[4] && Line[5])
+    {
+      prev_error = -1;
+      return -1;
+    }
+    else if (Line[1] && Line[2] && Line[3] && Line[4] && Line[5] && Line[6])
+    {
+      prev_error = 1;
+      return 1;
+    }
+  }
+  case 7:
+  {
+    prev_error = 0;
+    return 0;
+  }
+  default:
+  {
+    /*int temp = CountLeft() - CountRight();
+      if (temp < -1) {
+      prev_error = 8;
+      return 8;
+      } else if (temp > 1) {
+      prev_error = -8;
+      return -8;
+      } else*/
+    return 0;
+    break;
+  }
+  }
 }
 
-double SpeedPIDFilter(double PIDValue)
-{ // Slow down or spped up the robot when necessary
-    static double kP = 0, kI = 0, kD = 0;
-    static double prev_error = 0, prev_I = 0;
-    static int gain = 100;
-    double p = 0, i = 0, d = 0, pid_value;
+int CountLeft()
+{
+  int count = 0;
+  for (int i = 0; i < 3; i++)
+  {
+    if (Line[i])
+      count++;
+  }
+  return count;
+}
 
-    p = PIDValue;
-    i = prev_I;
-    d = p - prev_error;
+int CountRight()
+{
+  int count = 0;
+  for (int i = 4; i < 7; i++)
+  {
+    if (Line[i])
+      count++;
+  }
+  return count;
+}
 
-    pid_value = kP * p + kI * i + kD * d;
-    prev_error = p;
-    prev_I = i;
-
-    pid_value *= gain / 100;
-
-    constrain(pid_value, 0, INITIAL_SPEED);
-    return pid_value;
+int HighSignalCount()
+{ // Count the high signal that speed up the GetError function
+  int count = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    if (Line[i])
+    {
+      count++;
+    }
+  }
+  return count;
 }
 
